d3db9c65c909085923ff63a6d973c6ef86191aa8
kamalkumarkumawat114 <kamalkumarkumawat114@gmail.com>
2024-12-05 11:56:42 +0530

    Update game.js

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ const config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
-    backgroundColor: '#686c73',
+    backgroundColor: '#00B5E2',
     scene: {
         preload: preload,
         create: create
@@ -14,21 +14,21 @@ const game = new Phaser.Game(config);
 function preload() {
    
     this.load.image('image1', 'assets/image3.png'); 
-    this.load.image('image2', 'assets/light.png'); 
+    this.load.image('image2', 'assets/eagle2.png'); 
 }
 
 function create() {
     // Add a glow effect behind the first image
-    const glow = this.add.circle(400, 200, 150, 0xe90000, 0.3); 
-    glow.setScale(1.2); 
-    this.tweens.add({
-        targets: glow,
-        alpha: 0.5,
-        duration: 1000,
-        yoyo: true,
-        repeat: -1,
-        ease: 'Sine.easeInOut'
-    });
+    // const glow = this.add.circle(400, 200, 150, 0xe90000, 0.3); 
+    // glow.setScale(1.2); 
+    // this.tweens.add({
+    //     targets: glow,
+    //     alpha: 0.5,
+    //     duration: 1000,
+    //     yoyo: true,
+    //     repeat: -1,
+    //     ease: 'Sine.easeInOut'
+    // });
 
     
     const image1 = this.add.image(400, 200, 'image1');
@@ -51,8 +51,8 @@ function create() {
         ease: 'Bounce.easeOut'
     });
 
-    // Group all elements into a container
-    const container = this.add.container(0, 0, [glow, image1, text, image2]);
+    
+    const container = this.add.container(0, 0, [image1, text]);
 
     // Add interactivity: on hover, make the container rotate slightly
     container.setInteractive(new Phaser.Geom.Rectangle(0, 0, 800, 600), Phaser.Geom.Rectangle.Contains);
@@ -70,7 +70,7 @@ function create() {
     this.input.on('pointerdown', () => {
         this.tweens.add({
             targets: container,
-            scale: 0,
+            scale: 1,
             duration: 500,
             ease: 'Power2',
             onComplete: () => {
